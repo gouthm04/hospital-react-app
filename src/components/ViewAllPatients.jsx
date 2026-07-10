698c04fafd02ee6d576205ed8407bcd1d29a3c66
gouthm.04 <gouthm.04@gmail.com>
2026-07-10 18:09:20 +0530

    Fetch patient list from API

--- src/components/ViewAllPatients.jsx
+++ src/components/ViewAllPatients.jsx
@@ -1,26 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from './NavBar'
+import axios from 'axios'
 
 const ViewAllPatients = () => {
-    const [data,changeData] = useState(
-        [ 
-            { "patientId": "PT101", "name": "Arun Kumar", "age": 34, "gender": "Male", "bloodGroup": "O+", "doctor": "Dr. Meera", "room": "A101", "mobile": "9876543210" },
-            { "patientId": "PT102", "name": "Anjali Nair", "age": 27, "gender": "Female", "bloodGroup": "A+", "doctor": "Dr. Joseph", "room": "A102", "mobile": "9876543211" }, 
-            { "patientId": "PT103", "name": "Rahul Das", "age": 41, "gender": "Male", "bloodGroup": "B+", "doctor": "Dr. Suresh", "room": "B201", "mobile": "9876543212" }, 
-            { "patientId": "PT104", "name": "Sneha Roy", "age": 30, "gender": "Female", "bloodGroup": "AB+", "doctor": "Dr. Meera", "room": "B202", "mobile": "9876543213" }, 
-            { "patientId": "PT105", "name": "Vishnu Prasad", "age": 52, "gender": "Male", "bloodGroup": "O-", "doctor": "Dr. Anand", "room": "C301", "mobile": "9876543214" }, 
-            { "patientId": "PT106", "name": "Keerthana S", "age": 24, "gender": "Female", "bloodGroup": "A-", "doctor": "Dr. Joseph", "room": "C302", "mobile": "9876543215" }, 
-            { "patientId": "PT107", "name": "Nikhil Menon", "age": 38, "gender": "Male", "bloodGroup": "B-", "doctor": "Dr. Anand", "room": "D401", "mobile": "9876543216" }, 
-            { "patientId": "PT108",
-                 "name": "Priya Thomas",
-                 "age": 46,
-                 "gender": "Female",
-                 "bloodGroup": "O+",
-                 "doctor": "Dr. Suresh",
-                 "room": "D402",
-                 "mobile": "9876543217" 
-            } 
-        ]
+    const [data,changeData] = useState([])
+
+    const fetchData = () => {
+        
+            axios.get("https://raw.githubusercontent.com/anishpdm/demo-json-api/refs/heads/main/hospital-data.json")
+            .then(
+                (response) => {
+                    changeData(response.data)
+                }
+            ).catch()
+        }
+    
+
+    useEffect(
+        () => {
+            fetchData()
+        },[]
     )
   return (
     <div>
@@ -28,8 +27,8 @@ const ViewAllPatients = () => {
         <div className="container mt-3">
             <div className="row">
                 <div className="col-12">
-                    <table class="table table-hover table-bordered ">
-                        <thead class="table-primary">
+                    <table className="table table-hover table-bordered ">
+                        <thead className="table-primary">
                             <tr>
                             <th scope="col">Patient ID</th>
                             <th scope="col">Name</th>
