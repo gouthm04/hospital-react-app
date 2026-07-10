488eceeb55667a2253576677ca14b718c758de33
gouthm.04 <gouthm.04@gmail.com>
2026-07-10 18:23:26 +0530

    minior name updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import RegisterPatient from './components/RegisterPatient'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
 import ViewAllPatients from './components/ViewAllPatients'
 import SearchPatient from './components/SearchPatient'
-import DeletePatient from './components/DeletePatient'
+import DischargePatient from './components/DischargePatient'
 
 function App() {
 
@@ -16,7 +16,7 @@ function App() {
       <Route path='/' element={<RegisterPatient/>}/>
       <Route path='/view' element={<ViewAllPatients/>}/>
       <Route path='/search' element={<SearchPatient/>}/>
-      <Route path='/delete' element={<DeletePatient/>}/>
+      <Route path='/delete' element={<DischargePatient/>}/>
     </Routes>
     </BrowserRouter>
    </>
--- src/components/DischargePatient.jsx
+++ src/components/DischargePatient.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import NavBar from './NavBar'
 
-const DeletePatient = () => {
+const DischargePatient = () => {
   return (
     <div>
       <NavBar />
@@ -10,12 +10,12 @@ const DeletePatient = () => {
         <div className="row justify-content-center">
           <div className="col-md-6">
             <div className=" p-4">
-              <h3 className="text-center mb-3">Delete Patient</h3>
+              <h3 className="text-center mb-3">Discharge Patient</h3>
 
               <label className="form-label">Patient ID</label>
               <input type="text" className="form-control" placeholder="Enter Patient ID"/>
 
-              <button className="btn btn-danger mt-3">Delete</button>
+              <button className="btn btn-danger mt-3">Discharge</button>
             </div>
           </div>
         </div>
@@ -24,4 +24,4 @@ const DeletePatient = () => {
   )
 }
 
-export default DeletePatient
+export default DischargePatient
